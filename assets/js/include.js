abc38510a8ee31934ffba7986fd518888563f26b
Jan Escoda <janescoda49@gmail.com>
2025-12-18 12:46:58 +0100

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const includeElements = document.querySelectorAll("[data-include]");
+  const includeElements = document.querySelectorAll("[data-include]");
 
-    includeElements.forEach(el => {
-        const file = el.getAttribute("data-include");
+  includeElements.forEach(el => {
+    const file = el.getAttribute("data-include");
 
-        fetch(file)
-            .then(response => response.text())
-            .then(data => {
-                el.innerHTML = data;
-            });
-    });
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        el.innerHTML = data;
 
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const currentPage = window.location.pathname.split('/').pop() || 'index.html';
-
-  document.querySelectorAll('.nav-right a').forEach(link => {
-    if (link.getAttribute('href') === currentPage) {
-      link.classList.add('active');
-    }
+        //  ACTIVAR MENÚ DESPUÉS DE CARGARLO
+        activateMenu();
+      });
   });
 });
+
+function activateMenu() {
+  const currentPage =
+    window.location.pathname.split("/").pop() || "index.html";
+
+  document.querySelectorAll(".nav-right a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
 
